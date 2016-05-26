76f0d4856548b9a5f33d43c771a2c24b716a9f2b
SilentSupernova <chrisyoules93@gmail.com>
2016-05-26 22:01:45 +0100

    Menu, UI and started to work in mini map
    Signed-off-by: SilentSupernova <chrisyoules93@gmail.com>

---
+++ Game Jam/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+	public Camera mapCamera;
+
+	public float xPos;
+	public float yPos;
+	public float width;
+	public float height;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mapCamera = GetComponent<Camera>();
+		mapCamera.pixelRect = new Rect (Screen.width - xPos, Screen.height - yPos, width, height);
+	}
+	
+
+}
